c0c598692c90438c0c728669ef6d422e89f88378
septoon <tigran.darchinyan@gmail.com>
2018-12-29 09:43:36 +0300

    Change mail

--- mail.php
+++ mail.php
@@ -19,7 +19,7 @@ $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, 
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
 $mail->setFrom('Registr_777@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('Registr_777@mail.ru');     // Кому будет уходить письмо 
+$mail->addAddress('tigr_xxxl_999@mail.ru');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
